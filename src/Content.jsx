3ab9ff390120f8c4adea72d0d3c307e5afb46e14
jordanoboyle <jmcmcode@gmail.com>
2024-06-05 23:20:27 -0600

    Move handleHorseIndex to Content

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,14 +1,29 @@
 import { HorsesIndex } from "./HorsesIndex"
+import axios from "axios"
+import { useState, useEffect } from "react"
 
 
 
 export function Content() {
+  const [horses, setHorses] = useState([]);
 
+
+  //render basic data so we have a template
+  //pull data from axios and prove a render
+  //write a map function/loop to render all the data from the data base 
+  const handleHorsesIndex = () => {
+    console.log("pulling Horses Data");
+    axios.get("http://localhost:3000/horses.json").then((response) => {
+      console.log(response.data);
+      setHorses(response.data);
+    })
+    
+  } 
 
   return (
     <main>
       <h1>Welcome to React!</h1>
-      <HorsesIndex />
+      <HorsesIndex horses={horses} />
     </main>
   )
 }
--- src/HorsesIndex.jsx
+++ src/HorsesIndex.jsx
@@ -1,11 +1,22 @@
 import axios from "axios"
+import { useState } from "react";
+
 
 export function HorsesIndex() {
-  
-  
+
+
   return (
     <div>
       <h1>All Horses</h1>
+      {horses.map(horse => (
+        <div key={horse.id}>
+          <h2>Breed: {horse.breed} </h2>
+          <p>Build: {horse.build} </p>
+          <img id="horseImage" src={horse.image_url} />
+          <p>Color: {horse.color} </p>
+          <p>Price: {horse.price}</p>
+        </div>
+    ))}
     </div>
   );
 }
