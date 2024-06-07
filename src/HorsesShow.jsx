9dd9871d8d912c2e13c215271fdfb58467f2425f
jordanoboyle <jmcmcode@gmail.com>
2024-06-06 22:17:09 -0600

    skeleton for update for HorseShow

--- src/HorsesShow.jsx
+++ src/HorsesShow.jsx
@@ -8,7 +8,18 @@ export function HorsesShow(props) {
       <h1>Extra Inforamtion</h1>
       <p>ID: {props.horse.id}</p>
       <p>Breed: {props.horse.breed}</p>
-      <p>Color: {props.horse.breed}</p>
+      <p>Color: {props.horse.color}</p>
+      <br/>
+      <br/>
+      <br/>
+      <form>
+        <p>Breed: <input type="text" name="breed"/> </p>
+        <p>Build: <input type="text" name="build"/> </p>
+        <p>Color: <input type="text" name="color"/> </p>
+        <p>Price: <input type="text" name="price"/> </p>
+        <p>Image_url: <input type="text" name="image_url"/> </p>
+        <button>Submit Horse Information Update</button>
+      </form>
     </div>
   )
 }
